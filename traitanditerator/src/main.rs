5a47ac2a81e1b35a679e890c794778997e82b83c
Hsiaopan <hsiaopan@outlook.com>
2021-05-12 00:40:03 +0800

    trait and iterator

---
+++ traitanditerator/src/main.rs
@@ -0,0 +1,53 @@
+#[derive(Debug)]
+struct Counter {
+    length: usize,
+    count: usize,
+}
+
+impl Counter {
+    fn new(length: usize) -> Counter {
+        Counter { count: 0, length }
+    }
+}
+
+impl Iterator for Counter {
+    type Item = usize;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.count += 1;
+        if self.count <= self.length {
+            Some(self.count)
+        } else {
+            None
+        }
+    }
+}
+
+fn main() {
+    let mut counter = Counter::new(6);
+    println!("Counter just created: {:#?}", counter);
+
+    assert_eq!(counter.next(), Some(1));
+    assert_eq!(counter.next(), Some(2));
+    assert_eq!(counter.next(), Some(3));
+    assert_eq!(counter.next(), Some(4));
+    assert_eq!(counter.next(), Some(5));
+    assert_eq!(counter.next(), Some(6));
+
+    assert_eq!(counter.next(), None);
+    assert_eq!(counter.next(), None);
+    assert_eq!(counter.next(), None);
+
+    println!("Counter exhausted: {:?}", counter);
+
+    for number in Counter::new(8) {
+        println!("{}", number);
+    }
+
+    let sum_util_10: usize = Counter::new(10).sum();
+    assert_eq!(sum_util_10, 55);
+
+    let powers_of_2: Vec<usize> = Counter::new(8).map(|n| 2usize.pow(n as u32)).collect();
+    println!("{:?}", powers_of_2);
+    assert_eq!(powers_of_2, vec![2, 4, 8, 16, 32, 64, 128, 256]);
+}
