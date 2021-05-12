264f233e0fd223fd185c8f9eac245a3ae03b8a96
Hsiaopan <hsiaopan@outlook.com>
2021-05-12 22:48:34 +0800

    implement an iterator

---
+++ implementiterator/src/main.rs
@@ -0,0 +1,54 @@
+struct Groups<T> {
+    inner: Vec<T>,
+}
+
+impl<T> Groups<T> {
+    fn new(inner: Vec<T>) -> Self {
+        Groups { inner }
+    }
+}
+
+impl<T: PartialEq> Iterator for Groups<T> {
+    type Item = Vec<T>;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.inner.is_empty() {
+            return None;
+        }
+
+        let mut cursor = 1;
+        let first = &self.inner[0];
+        for element in &self.inner[1..] {
+            if element == first {
+                cursor += 1;
+            } else {
+                break;
+            }
+        }
+
+        let items = self.inner.drain(0..cursor).collect();
+
+        Some(items)
+    }
+}
+
+fn main() {
+    let input = vec![4, 1, 1, 2, 1, 3, 3, 3, -2, -2, -1, 5, 5];
+
+    let output = Groups::new(input).into_iter().collect::<Vec<Vec<_>>>();
+    assert_eq!(
+        output,
+        vec![
+            vec![4],
+            vec![1, 1],
+            vec![2],
+            vec![1],
+            vec![3, 3, 3],
+            vec![-2, -2],
+            vec![-1],
+            vec![5, 5],
+        ]
+    );
+
+    println!("{:?}", output);
+}
