b1c76ebc1fb72177e228ba054693527fa4a2609c
Hsiaopan <hsiaopan@outlook.com>
2021-05-17 23:48:42 +0800

    multi-fn

--- rectangles/src/main.rs
+++ rectangles/src/main.rs
@@ -4,9 +4,13 @@ struct Rectangle {
     height: u32,
 }
 
-impl Rectangle {
+impl Rectangle {   // 定义方法
     fn area(&self) -> u32 {
         self.width * self.height
+    }
+
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
     }
 }
 
@@ -16,10 +20,23 @@ fn main() {
         height: 50,
     };
 
+    let rec2 = Rectangle {
+        width: 35,
+        height:48,
+    };
+
+    let rec3 = Rectangle {
+        width:28,
+        height: 45,
+    };
+
     println!("{:#?}", rec1); // Debug 输出格式，需要增加 Debug 的trait
 
     println!(
         "The area of the rectangle is {} square pixels.",
         rec1.area()
     );
+
+    println!("Can rec1 hold rec2? {}", rec1.can_hold(&rec2));
+    println!("Can rec1 hold rec3? {}", rec1.can_hold(&rec3));
 }
