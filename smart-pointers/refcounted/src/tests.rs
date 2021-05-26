d656876f218c5fddff44341991963f11f2f020ef
Hsiaopan <hsiaopan@outlook.com>
2021-05-26 22:56:20 +0800

    reference counting.

---
+++ smart-pointers/refcounted/src/tests.rs
@@ -0,0 +1,18 @@
+use crate::List::{Cons, Nil};
+use std::rc::Rc;
+
+#[test]
+fn it_works() {
+    let a = Rc::new(Cons(5, Rc::new(Cons(10, Rc::new(Nil)))));
+    println!("Count after creating a = {}", Rc::strong_count(&a));
+
+    let b = Cons(3, Rc::clone(&a));
+    println!("Count after creating b = {}", Rc::strong_count(&a));
+
+    {
+        let c = Cons(4, Rc::clone(&a));
+        println!("Count after creating c = {}", Rc::strong_count(&a));
+    }
+
+    println!("Count after c goes out of scope = {}", Rc::strong_count(&a));
+}
